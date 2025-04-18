de6f07650eee586446b71de97471e81e25509295
Patrick Christoph <pchristoph87@googlemail.com>
2025-04-18 15:52:15 +0200

    optimized logging for preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -25,6 +25,8 @@ def load_meta_data(raw_file_path: str) -> pd.DataFrame:
     meta["value"] = [v.split(", ") if type(v) == str and "," in v else v for v in meta["value"]]
     meta = meta.explode("value")
 
+    print("Metadata loaded.")
+
     return meta
 
 
@@ -45,6 +47,8 @@ def rectify_meta_attributes(meta: pd.DataFrame) -> pd.DataFrame:
     }
 
     meta["attribute"] = meta["attribute"].replace(attribute_renaming)
+
+    print("Metadata rectified.")
 
     return meta
 
@@ -67,43 +71,46 @@ def convert_unknown_values_to_null(df: pd.DataFrame, meta: pd.DataFrame) -> pd.D
         if attribute in df:
             df[attribute] = df[attribute].replace(float(unknown_value), np.nan)
 
+    print(f"Converted {len(unknown_values) + 3} unknown values to null.")
+
     return df
 
 
-def identify_invalid_oor_values(meta: pd.DataFrame, df: pd.DataFrame, log: bool = False) -> pd.DataFrame:
+def identify_invalid_out_of_range_values(meta: pd.DataFrame, df: pd.DataFrame, log_details: bool = False) -> pd.DataFrame:
     """
-    Identify out-of-range (oor) values in the dataset based on metadata.
+    Identify out-of-range values in the dataset based on metadata.
 
     Compares actual values in the dataset against expected values defined in the metadata.
 
     :param meta: Metadata with valid attribute values.
     :param df: Dataset to check for invalid values.
-    :param log: Whether to print details of each out-of-range value found.
+    :param log_details: Whether to print details of each out-of-range value found.
     :return: Attributes and their out-of-range values.
     """
     categorical_values = meta[meta["value"] != "…"][["attribute", "value"]]
     categorical_values = categorical_values.rename(columns={"value": "meta_value"})
 
-    invalid_oor_values = []
+    total_invalid_oor_values = []
 
     for attribute in categorical_values["attribute"].unique():
         if attribute in df:
-            pop_vc = df[attribute].value_counts().reset_index()
-            pop_vc = pop_vc.merge(
+            attribute_value_counts = df[attribute].value_counts().reset_index()
+            attribute_value_counts = attribute_value_counts.merge(
                 categorical_values[categorical_values["attribute"] == attribute]["meta_value"],
                 left_on=attribute,
                 right_on="meta_value",
                 how="left",
             )
 
-            if sum(pop_vc["meta_value"].isnull()) > 0:
-                for out_of_range_value in pop_vc[pop_vc["meta_value"].isnull()].itertuples():
-                    if log:
+            invalid_oor_values = attribute_value_counts[attribute_value_counts["meta_value"].isnull()]
+            if len(invalid_oor_values) > 0:
+                for out_of_range_value in invalid_oor_values.itertuples():
+                    if log_details:
                         print(f"attribute: {attribute}, invalid value: {out_of_range_value[1]}, "
                               f"affected rows: {out_of_range_value.count}")
-                    invalid_oor_values.append([attribute, out_of_range_value[1]])
+                    total_invalid_oor_values.append([attribute, out_of_range_value[1]])
 
-    return pd.DataFrame(data=invalid_oor_values, columns=["attribute", "value"])
+    return pd.DataFrame(data=total_invalid_oor_values, columns=["attribute", "value"])
 
 
 def convert_invalid_values_to_null(df: pd.DataFrame, meta: pd.DataFrame) -> pd.DataFrame:
@@ -119,10 +126,12 @@ def convert_invalid_values_to_null(df: pd.DataFrame, meta: pd.DataFrame) -> pd.D
     df.loc[df["geburtsjahr"] == 0, "geburtsjahr"] = np.nan
     df.loc[df["anz_personen"] == 0, "anz_personen"] = np.nan
 
-    invalid_oor_values = identify_invalid_oor_values(meta, df, log=True)
+    invalid_oor_values = identify_invalid_out_of_range_values(meta, df, log_details=False)
     for attribute, invalid_oor_value in zip(invalid_oor_values["attribute"], invalid_oor_values["value"]):
         if attribute in df:
             df[attribute] = df[attribute].replace(invalid_oor_value, np.nan)
+
+    print(f"Converted {len(invalid_oor_values) + 2} invalid values to null.")
 
     return df
 
@@ -137,7 +146,10 @@ def remove_records_with_insufficient_data(df: pd.DataFrame, max_missing_values: 
     :param max_missing_values: Maximum allowed number of missing values per record.
     :return: Dataset without rows that have too many missing feature values.
     """
-    df =df[~(df.isnull().sum(axis=1) >= max_missing_values)]
+    insufficient_records = df.isnull().sum(axis=1) >= max_missing_values
+    df = df[~insufficient_records]
+
+    print(f"Removed {sum(insufficient_records)} records with over {max_missing_values} missing feature values.")
 
     return df
 
@@ -156,7 +168,7 @@ def remove_features_with_high_missing_values_ratio(df: pd.DataFrame, max_ratio: 
     columns_with_high_ratio = list(missing_ratios[missing_ratios >= max_ratio].index)
 
     df = df.drop(columns=columns_with_high_ratio)
-    print(f"\nRemoved {len(columns_with_high_ratio)} features due to high missing ratio:"
+    print(f"Removed {len(columns_with_high_ratio)} features with over {max_ratio * 100}% missing features:"
           f"\n{", ".join(columns_with_high_ratio)}")
 
     return df
@@ -181,8 +193,7 @@ def impute_features_with_median_or_mode(df: pd.DataFrame) -> pd.DataFrame:
 
         df[column] = df[column].fillna(fill_value)
 
-    print(f"\nReplaced missing values in {len(columns_with_missing_values)} features:"
-          f"\n{", ".join(columns_with_missing_values)}")
+    print(f"Replaced missing values in {len(columns_with_missing_values) + 4} features.")
 
     return df
 
@@ -243,6 +254,8 @@ def split_formative_youth_years(df: pd.DataFrame, meta: pd.DataFrame) -> pd.Data
 
     df = df.drop(columns=["praegende_jugendjahre"])
 
+    print("Extracted decade and cultural orientation as separate features.")
+
     return df
 
 
@@ -257,6 +270,8 @@ def split_international_cameo(df: pd.DataFrame) -> pd.DataFrame:
     df["cameo_life_phase"] = [int(str(v)[1]) if v != -1 and pd.notnull(v) else v for v in df["cameo_intl_2015"]]
 
     df = df.drop(columns=["cameo_intl_2015"])
+
+    print("Extracted wealth and life phase as separate features.")
 
     return df
 
@@ -273,7 +288,7 @@ def remove_features(df: pd.DataFrame, feature_configs: Dict[str, List[str]], rem
     for removal_type in removal_types:
         features = feature_configs[removal_type]
         df = df.drop(columns=features)
-        print(f"\nRemoved {len(features)} {removal_type} features: {", ".join(features)}")
+        print(f"Removed {len(features)} {removal_type} features.")
 
     return df
 
@@ -295,7 +310,7 @@ def encode_binary_features(df: pd.DataFrame, binary_features: List[str]) -> pd.D
         else:
             raise ValueError(f"Feature {feature} has more than two unique values: {unique_values}")
 
-    print(f"Encoded {len(binary_features)} binary features: {", ".join(binary_features)}")
+    print(f"Encoded {len(binary_features)} binary features.")
 
     return df
 
@@ -326,14 +341,14 @@ def encode_categorical_features(df: pd.DataFrame, categorical_features: List[str
 
     dummy_variables = list(set(df).difference(non_categorical_columns))
 
-    print(f"One-hot encoded {len(categorical_features)} categorical features into {len(dummy_variables)} dummy "
-          f"variables: {", ".join(categorical_features)}")
+    print(f"One-hot encoded {len(categorical_features)} categorical features "
+          f"into {len(dummy_variables)} dummy variables.")
 
     return df, dummy_variables
 
 
 def scale_numeric_features(
-        df: pd.DataFrame, numeric_features: List[str], scaler: Optional[StandardScaler] = None
+        df: pd.DataFrame, numeric_features: List[str], df_name: str, scaler: Optional[StandardScaler] = None
 ) -> Tuple[pd.DataFrame, StandardScaler]:
     """
     Scales specified numeric features in the dataset using StandardScaler.
@@ -345,6 +360,7 @@ def scale_numeric_features(
 
     :param df: Dataset containing the numeric features to scale.
     :param numeric_features: Column names in the dataset that are numeric.
+    :param df_name: Name of the dataset for logging.
     :param scaler: An optional pre-fitted StandardScaler.
     :returns: - Dataset with the original numeric features replaced by the scaled versions.
               - StandardScaler used for scaling (either the provided one or a newly fitted one).
@@ -359,12 +375,16 @@ def scale_numeric_features(
     df = df.drop(columns=list(numeric_features))
     df = pd.concat([df, scaled_features], axis=1)
 
-    print(f"Scaled {len(numeric_features)} numeric features: {", ".join(numeric_features)}")
+    print(f"Scaled {len(numeric_features)} numeric features from {df_name} dataset.")
 
     return df, scaler
 
 
-def clean_data(df: pd.DataFrame, meta: pd.DataFrame, feature_config: Dict[str, List[str]]) -> pd.DataFrame:
+def clean_data(
+        df: pd.DataFrame, meta: pd.DataFrame, feature_config: Dict[str, List[str]], df_name: str
+) -> pd.DataFrame:
+    print(f"\nStart cleaning {df_name} dataset...")
+
     df = remove_features(df, feature_config, ["irrelevant"])
 
     df = convert_unknown_values_to_null(df, meta)
@@ -381,7 +401,11 @@ def clean_data(df: pd.DataFrame, meta: pd.DataFrame, feature_config: Dict[str, L
     return df
 
 
-def encode_data(df: pd.DataFrame, feature_config: Dict[str, List[str]]) -> Tuple[pd.DataFrame, List[str]]:
+def encode_data(
+        df: pd.DataFrame, feature_config: Dict[str, List[str]], df_name: str
+) -> Tuple[pd.DataFrame, List[str]]:
+    print(f"\nStart encoding {df_name} dataset...")
+
     df = encode_binary_features(df, feature_config["binary"])
     df, dummy_variables = encode_categorical_features(df, feature_config["categorical"])
 
@@ -397,11 +421,13 @@ def complete_dummy_variables(df, dummy_cols, other_dummy_cols):
     return df
 
 
-def sync_features(
+def align_features(
         df1: pd.DataFrame, df2: pd.DataFrame, dummy_cols_df1: List[str], dummy_cols_df2: List[str]
 ) -> Tuple[pd.DataFrame, pd.DataFrame, Set[str]]:
     df1 = complete_dummy_variables(df1, dummy_cols_df1, dummy_cols_df2)
     df2 = complete_dummy_variables(df2, dummy_cols_df2, dummy_cols_df1)
+
+    print("\nAligned categorial features after one-hot encoding.")
 
     dummy_variables = set(dummy_cols_df1) | (set(dummy_cols_df2))
 
@@ -409,10 +435,12 @@ def sync_features(
     df1 = df1[feature_intersection]
     df2 = df2[feature_intersection]
 
+    print("Aligned numerical features.")
+
     return df1, df2, dummy_variables
 
 
-def preprocess_data(df1: pd.DataFrame, df2: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def preprocess_data(df1: pd.DataFrame, df2: pd.DataFrame, names: Tuple[str, str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """
     Preprocesses the datasets through a series of data cleaning, feature engineering,
     encoding, and scaling steps.
@@ -421,6 +449,7 @@ def preprocess_data(df1: pd.DataFrame, df2: pd.DataFrame) -> Tuple[pd.DataFrame,
 
     :param df1: First raw input dataset to be preprocessed.
     :param df2: Second raw input dataset to be preprocessed.
+    :param names: Names of the datasets for logging.
     :returns: - First preprocessed dataset.
               - Second preprocessed dataset.
     """
@@ -429,29 +458,33 @@ def preprocess_data(df1: pd.DataFrame, df2: pd.DataFrame) -> Tuple[pd.DataFrame,
 
     with open("feature_config.json", "r") as f:
         feature_config = json.load(f)
+    print("Feature config loaded.")
 
-    df1 = clean_data(df1, meta, feature_config)
-    df2 = clean_data(df2, meta, feature_config)
+    df1 = clean_data(df1, meta, feature_config, names[0])
+    df2 = clean_data(df2, meta, feature_config, names[1])
 
-    df1, dummy_variables_df1 = encode_data(df1, feature_config)
-    df2, dummy_variables_df2 = encode_data(df2, feature_config)
+    df1, dummy_variables_df1 = encode_data(df1, feature_config, names[0])
+    df2, dummy_variables_df2 = encode_data(df2, feature_config, names[1])
 
-    df1, df2, dummy_variables = sync_features(df1, df2, dummy_variables_df1, dummy_variables_df2)
+    df1, df2, dummy_variables = align_features(df1, df2, dummy_variables_df1, dummy_variables_df2)
 
+    print(f"\nStart scaling datasets...")
     non_numeric_features = set(dummy_variables) | set(feature_config["binary"])
     numeric_features = [feature for feature in df1.columns if feature not in non_numeric_features]
-    df1, scaler = scale_numeric_features(df1, numeric_features)
-    df2, _ = scale_numeric_features(df2, numeric_features, scaler)
+    df1, scaler = scale_numeric_features(df1, numeric_features, names[0])
+    df2, _ = scale_numeric_features(df2, numeric_features, names[1], scaler)
+
+    print(f"Finish preprocessing for both datasets.")
 
     return df1, df2
 
 
 if __name__=="__main__":
-    customer = pd.read_csv("../data/Udacity_CUSTOMERS_052018.csv", sep=";", nrows=10000)
+    customer = pd.read_csv("../data/Udacity_CUSTOMERS_052018.csv", sep=";", nrows=100000)
     customer.columns = customer.columns.str.lower()
     customer = customer.drop(columns=["customer_group", "online_purchase", "product_group"])
 
-    population = pd.read_csv("../data/Udacity_AZDIAS_052018.csv", sep=";", nrows=10000)
+    population = pd.read_csv("../data/Udacity_AZDIAS_052018.csv", sep=";", nrows=100000)
     population.columns = population.columns.str.lower()
 
-    population, customer = preprocess_data(population, customer)
+    population, customer = preprocess_data(population, customer, ("population", "customer"))
